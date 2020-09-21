1a5e69aafe430409eb88b90eac9c81521cea2eca
wangyuheng <wangyuheng@outlook.com>
2020-09-21 11:43:35 +0800

    调试代码生成

---
+++ generator/src/test/java/ai/care/arc/generator/JavaCodeGeneratorHandlerTest.java
@@ -0,0 +1,28 @@
+package ai.care.arc.generator;
+
+import ai.care.arc.generator.conf.CodeGenConfig;
+import ai.care.arc.generator.conf.CodeGenOperation;
+import ai.care.arc.generator.conf.CodeGenStrategy;
+import ai.care.arc.generator.conf.CodeGenType;
+import ai.care.arc.generator.io.CodeWriter;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Collections;
+
+@Ignore
+public class JavaCodeGeneratorHandlerTest {
+
+    @Test
+    public void gene_java_code() throws IOException {
+        ClassPathResource schema = new ClassPathResource("schema.graphqls");
+        CodeWriter codeWriter = new CodeWriter(Paths.get(".", "/src/main/java"));
+        CodeGenConfig config = new CodeGenConfig(Collections.singletonList(new CodeGenStrategy(CodeGenType.REPO, CodeGenOperation.SKIP_IF_EXISTED)));
+
+        new JavaCodeGeneratorHandler(codeWriter, config).handle(schema.getInputStream(), "ai.care.generatorsample.generated");
+    }
+
+}
