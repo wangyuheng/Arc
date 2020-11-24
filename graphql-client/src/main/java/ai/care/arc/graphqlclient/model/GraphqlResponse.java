f152bdf3297e0e07180e5355f1e3396e2a8e14d4
wangyuheng <wangyuheng@outlook.com>
2020-11-24 11:47:06 +0800

    pojo model

---
+++ graphql-client/src/main/java/ai/care/arc/graphqlclient/model/GraphqlResponse.java
@@ -0,0 +1,42 @@
+package ai.care.arc.graphqlclient.model;
+
+import java.util.List;
+
+/**
+ * graphql 返回值
+ *
+ * @author yuheng.wang
+ */
+public class GraphqlResponse<T> {
+
+    private T data;
+    private List<GraphqlError> errors;
+
+    public GraphqlResponse(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public List<GraphqlError> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<GraphqlError> errors) {
+        this.errors = errors;
+    }
+
+    @Override
+    public String toString() {
+        return "GraphqlResponse{" +
+                "data=" + data +
+                ", errors=" + errors +
+                '}';
+    }
+}
