1b1bc0e43f8115aba20539dfc12c80828494ecf7
junhao.chen <junhao.chen@yitu-inc.com>
2020-11-30 21:14:37 +0800

    add todo and test

--- graphql/src/main/java/ai/care/arc/graphql/exception/CustomDataFetcherExceptionHandler.java
+++ graphql/src/main/java/ai/care/arc/graphql/exception/CustomDataFetcherExceptionHandler.java
@@ -9,6 +9,7 @@ public class CustomDataFetcherExceptionHandler extends SimpleDataFetcherExceptio
     @Override
     public DataFetcherExceptionHandlerResult onException(DataFetcherExceptionHandlerParameters handlerParameters) {
         Throwable exception = handlerParameters.getException();
+        //todo 提供新的异常处理 隐藏location信息和exception整体信息 只保留message并额外添加errorCode
         if (exception instanceof CustomException) {
             return super.onException(DataFetcherExceptionHandlerParameters
                     .newExceptionParameters()
