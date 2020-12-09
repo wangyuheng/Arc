db226092d2ea24e0fcbc185963d79a86094cac4d
wangyuheng <wangyuheng@outlook.com>
2020-12-09 22:18:39 +0800

    arc maven plugin for generator

---
+++ maven-plugin/src/main/java/com/github/yituhealthcare/arc/mavenplugin/GeneratorAliasMojo.java
@@ -0,0 +1,13 @@
+package com.github.yituhealthcare.arc.mavenplugin;
+
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * Alias Mojo For {@link GeneratorMojo}
+ *
+ * @author yuheng.wang
+ */
+@Mojo(name = "-_-")
+public class GeneratorAliasMojo extends GeneratorMojo {
+
+}
