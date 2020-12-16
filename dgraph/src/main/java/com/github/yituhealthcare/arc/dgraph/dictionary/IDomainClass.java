e91fe2510caef530e4062e41170edd59df60419b
wangyuheng <wangyuheng@outlook.com>
2020-12-16 17:59:28 +0800

    rename IDgraphType to IDomainClass

--- dgraph/src/main/java/com/github/yituhealthcare/arc/dgraph/dictionary/IDomainClass.java
+++ dgraph/src/main/java/com/github/yituhealthcare/arc/dgraph/dictionary/IDomainClass.java
@@ -3,7 +3,7 @@ package com.github.yituhealthcare.arc.dgraph.dictionary;
 /**
  * 通过json在序列化时放置domainClass较复杂，暂时通过interface实现
  */
-public interface IDgraphType {
+public interface IDomainClass {
     default String getDomainClass() {
         return this.getClass().getName();
     }
